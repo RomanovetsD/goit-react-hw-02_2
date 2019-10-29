94cfc26b255de8a2c35442e4cc2a6483c15a0114
RomanovetsD <hungry_hungry@ukr.net>
2019-10-29 18:05:16 +0200

    Updates

--- precache-manifest.819e59ae7cac9cf0797f5e0c00b0b7ef.js
+++ precache-manifest.819e59ae7cac9cf0797f5e0c00b0b7ef.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b7c8ce72fcb75a0a875ab19b1a5fc231",
+    "revision": "f8b406afe58511c4dc8e47c11ca95b68",
     "url": "/goit-react-hw-02_bank_acount/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-02_bank_acount/static/css/2.da529cc7.chunk.css"
   },
   {
-    "revision": "b427220453e68df85f0d",
+    "revision": "13a8817d66f818345207",
     "url": "/goit-react-hw-02_bank_acount/static/css/main.f3d36b55.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-02_bank_acount/static/js/2.99f115ea.chunk.js"
   },
   {
-    "revision": "b427220453e68df85f0d",
-    "url": "/goit-react-hw-02_bank_acount/static/js/main.a146955f.chunk.js"
+    "revision": "13a8817d66f818345207",
+    "url": "/goit-react-hw-02_bank_acount/static/js/main.4444982d.chunk.js"
   },
   {
     "revision": "886ef098da80ab3a59fd",
